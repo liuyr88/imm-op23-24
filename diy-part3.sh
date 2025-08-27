7fc215db031826884edb0ef5959b497ea428f0c5
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2025-08-27 13:38:31 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,1 +1,3 @@
-
+#Modify default IP 用户名  密码
+sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/Melt-OpenWrt/g' package/base-files/files/bin/config_generate
